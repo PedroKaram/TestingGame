4d00d58b5715317331d571506fb776ae3e707001
PedroKaram <pedrokaram4005@gmail.com>
2024-02-17 17:54:43 -0300

    Writing unit tests for RegisterPoints class methods, testing VIP users

--- src/test/java/RegisterPointsTest.java
+++ src/test/java/RegisterPointsTest.java
@@ -43,4 +43,43 @@ public class RegisterPointsTest {
 
         assertEquals(1, user.points);
     }
+
+    @Test
+    public void comentVIPTest() {
+        User user = new User();
+        user.vip = true;
+
+        BonusCalculator bc = new BonusCalculator();
+
+        RegisterPoints rp = new RegisterPoints(bc);
+        rp.coment(user);
+
+        assertEquals(15, user.points);
+    }
+
+    @Test
+    public void createTopicVIPTest() {
+        User user = new User();
+        user.vip = true;
+
+        BonusCalculator bc = new BonusCalculator();
+
+        RegisterPoints rp = new RegisterPoints(bc);
+        rp.createTopic(user);
+
+        assertEquals(25, user.points);
+    }
+
+    @Test
+    public void likeVIPTest() {
+        User user = new User();
+        user.vip = true;
+
+        BonusCalculator bc = new BonusCalculator();
+
+        RegisterPoints rp = new RegisterPoints(bc);
+        rp.like(user);
+
+        assertEquals(5, user.points);
+    }
 }
